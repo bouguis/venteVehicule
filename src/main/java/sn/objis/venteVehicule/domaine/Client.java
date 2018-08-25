3be967316506721c77f021913191c4d88d8dafe2
Bouna Diawara <bouguis@gmail.com>
2018-08-25 14:16:41 +0200

    ajout des proprietes, constructeur, getters et setters a la classe Client

--- src/main/java/sn/objis/venteVehicule/domaine/Client.java
+++ src/main/java/sn/objis/venteVehicule/domaine/Client.java
@@ -8,5 +8,104 @@ package sn.objis.venteVehicule.domaine;
  *
  */
 public class Client {
+	//Séction proprieés
+	private long idClient;
+	private String nom;
+	private String prenom;
+	private String sexe;
+	private String adresse;
+	private String email;
+	private String tel;
+	
+	//Constructeurs
+	/**
+	 *Constructeur sans parametre 
+	 */
+	public Client() {
+		super();
+	}
+
+	/**
+	 * @param nom
+	 * @param prenom
+	 * @param sexe
+	 * @param adresse
+	 * @param email
+	 * @param tel
+	 */
+	public Client(String nom, String prenom, String sexe, String adresse, String email, String tel) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.sexe = sexe;
+		this.adresse = adresse;
+		this.email = email;
+		this.tel = tel;
+	}
+	
+	//Getters et Setters
+
+	public long getIdClient() {
+		return idClient;
+	}
+
+	public void setIdClient(long idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
