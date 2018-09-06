445e58ab8c16488b75afe7d3a2df9d616ba3b6b3
Rosine FAYE <rosinefaye@gmail.com>
2018-09-06 16:42:23 +0000

    rectification du pattern abstract factory

--- src/main/java/sn/objis/venteVehicule/service/CreationAutoElectrique.java
+++ src/main/java/sn/objis/venteVehicule/service/CreationAutoElectrique.java
@@ -1,13 +1,25 @@
 package sn.objis.venteVehicule.service;
 
+import java.sql.Connection;
+
+import sn.objis.venteVehicule.dao.IDaoAutomobileImpl;
 import sn.objis.venteVehicule.domaine.Automobile;
 
 public class CreationAutoElectrique implements CreationAuto{
+		
+		private IDaoAutomobileImpl dao;
+		Connection con;
 
-	@Override
-	public void creerAutomobile(Automobile auto) {
-		
-		
-	}
+		public CreationAutoElectrique(Connection con) {
+			super();
+			this.con = con;
+			dao = new IDaoAutomobileImpl(con);
+		}
+
+
+		@Override
+		public void creerAutomobile(Automobile auto) {
+			dao.ajouter(auto);
+		}
 
 }
--- src/main/java/sn/objis/venteVehicule/service/CreationAutoEssence.java
+++ src/main/java/sn/objis/venteVehicule/service/CreationAutoEssence.java
@@ -1,13 +1,24 @@
 package sn.objis.venteVehicule.service;
 
+import java.sql.Connection;
+import sn.objis.venteVehicule.dao.IDaoAutomobileImpl;
 import sn.objis.venteVehicule.domaine.Automobile;
 
 public class CreationAutoEssence implements CreationAuto{
+	
+	private IDaoAutomobileImpl dao;
+	Connection con;
+
+	public CreationAutoEssence(Connection con) {
+		super();
+		this.con = con;
+		dao = new IDaoAutomobileImpl(con);
+	}
+
 
 	@Override
 	public void creerAutomobile(Automobile auto) {
-		
-		
+		dao.ajouter(auto);
 	}
-
-}
+	}
+	
