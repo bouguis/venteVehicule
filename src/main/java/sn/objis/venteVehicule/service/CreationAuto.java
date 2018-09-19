e82246b4c0fafbde511d560efdb29b21655628bf
Rosine FAYE <rosinefaye@gmail.com>
2018-09-19 00:50:05 +0000

    nieme modification

--- src/main/java/sn/objis/venteVehicule/service/CreationAuto.java
+++ src/main/java/sn/objis/venteVehicule/service/CreationAuto.java
@@ -2,6 +2,10 @@ package sn.objis.venteVehicule.service;
 import sn.objis.venteVehicule.domaine.Automobile;
 
 
+/**
+ * @author HP
+ *
+ */
 public interface CreationAuto {
 	
 	public void creerAutomobile(Automobile auto);
